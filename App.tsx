239ac09c7e046448c8da754e0ed96271d43f0ebb
舟木正憲 <funakimfunaki@gmail.com>
2020-02-11 17:44:07 +0900

    レイアウトの区画整理

--- App.tsx
+++ App.tsx
@@ -1,26 +1,30 @@
 import React, { FC } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ViewStyle } from 'react-native';
 
-const styles = StyleSheet.create({
+interface Style {
+  container: ViewStyle;
+  itemContainer: ViewStyle;
+}
+
+const styles = StyleSheet.create<Style>({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
 });
 
 const App: FC = () => {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          height: 100,
-          width: '100%',
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-      />
+      <View style={styles.itemContainer} />
     </View>
   );
 };
